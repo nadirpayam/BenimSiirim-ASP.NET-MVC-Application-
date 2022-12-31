b1497019d0c4c5ae41fa2d2be26033af9be97483
Nadir Payam <nadirpayam@hotmail.com>
2022-12-31 16:29:31 +0300

    Database added

---
+++ DataAccessLayer/Abstract/ICategoryDal.cs
@@ -0,0 +1,21 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+    interface ICategoryDal
+    {
+        //CRUD
+        List<PoetryCategories> List();
+
+        void Insert(PoetryCategories categories);
+
+        void Update(PoetryCategories categories);
+
+        void Delete(PoetryCategories categories);
+    }
+}
---
+++ DataAccessLayer/Concrete/Repositories/CategoryRepository.cs
@@ -0,0 +1,38 @@
+﻿using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete.Repositories
+{
+    public class CategoryRepository : ICategoryDal
+    {
+        Context c = new Context();
+        DbSet<PoetryCategories> _object;
+        public void Delete(PoetryCategories categories)
+        {
+            _object.Remove(categories);
+            c.SaveChanges();
+        }
+
+        public void Insert(PoetryCategories categories)
+        {
+            _object.Add(categories);
+            c.SaveChanges();
+        }
+
+        public List<PoetryCategories> List()
+        {
+            return _object.ToList(); // verileri listeler
+        }
+
+        public void Update(PoetryCategories categories)
+        {
+            c.SaveChanges();
+        }
+    }
+}
